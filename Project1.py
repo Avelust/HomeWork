e7d91351acd4eea4428bf69e0a75eff4ba431207
Avelust <125144503+Avelust@users.noreply.github.com>
2023-02-11 19:29:01 +0200

    all delete'

--- Project1.py
+++
@@ -1,6 +0,0 @@
-while True:
-    number_one=input("Введіть ваше число: ")
-    number_two=input("Введіть ваше друге число: ")
-    if number_one == 'end' or number_two == 'end':
-        break
-    print((pow(int(number_one),int(number_two))))
